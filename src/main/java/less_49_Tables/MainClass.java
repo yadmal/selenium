ad76385b2a59f5f991f487c083566800ec9b8283
YashkinDmitry <yashkin23@gmail.com>
2020-01-31 15:59:22 +0300

    less_49_Tables

--- src/main/java/less_49_Tables/MainClass.java
+++ src/main/java/less_49_Tables/MainClass.java
@@ -1,9 +1,10 @@
+package less_49_Tables;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MainClass {
--- src/main/java/less_49_Tables/Table.java
+++ src/main/java/less_49_Tables/Table.java
@@ -1,3 +1,5 @@
+package less_49_Tables;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
