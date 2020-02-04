d314f347c049ed9336bbe17b866420154f5d402f
YashkinDmitry <yashkin23@gmail.com>
2020-02-05 00:03:04 +0300

    70 Assert in JUnit

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -4,20 +4,27 @@ public class MainClassTest {
     // Метод будет выполнен перед выполнением всех остальных тестовых методов.
     // Позволяет подготовить среду перед использованием остальных тестовых методов
     @BeforeClass
-    public void beforeClassMethod(){
-
+    public static void beforeClassMethod(){
+        System.out.println("@BeforeClass");
     }
 
     // будет запускаться перед каждым тестовым методом
     @Before
     public void setUp(){
-
+        System.out.println("@Before");
     }
 
     // непосредственно сам тест
     @Test
     public void method1(){
+        Assert.assertTrue("Values aren't equal", 1 == 1);
+        Assert.assertFalse(1 == 2);
 
+        Assert.assertNull(null); // в () можно указать метод, который должен вернуть null
+        Assert.assertNotNull(new Object()); // ожидает получить объект
+
+        Assert.assertEquals("Values aren't equal 2",10, 5+5); // ожидаемый результат, реальный результат
+        Assert.assertNotEquals(10, 7+9); // ожидаемый результат, реальный результат
     }
 
     @Test
@@ -29,12 +36,12 @@ public class MainClassTest {
     // будет выполняться после каждого тестового метода
     @After
     public void tearDown(){
-
+        System.out.println("@After");
     }
 
     // Будет выполняться после всех методов
     @AfterClass
-    public void afterClassMethod(){
-
+    public static void afterClassMethod(){
+        System.out.println("@AfterClass");
     }
 }
